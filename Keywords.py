e1daca4883f8ed2249128284da3f41e036a3e1bb
wasekaiqbal <iqbalwaseka@gmail.com>
2025-06-23 22:00:20 +0600

    Create Keywords.py

---
+++ Keywords.py
@@ -0,0 +1,31 @@
+'''print("finding the given letter")
+
+word = str(input("enter a word: "))
+letter = str(input("enter the letter you want to check: "))
+
+for i in word:
+    if (i == letter ):
+        print(letter, "is found in", word)
+        break
+    else:
+        print(letter, "is not found in ",word)'''
+
+print("the selected students for next round from roll number 0-9")
+
+'''var = 10
+while var > 0:
+    var = var -1
+    if var == 5:
+        continue
+    print("the selected student's rol number is", var)
+print(" 5 has not been  selected")'''
+
+print("motivatng you about choosing goals")
+
+goal = str(input("enter your goal in life(if you dont have i dont know what to do. so enter blank string): "))
+
+if goal == "":
+    pass
+else:
+    print("you wil be successful, i believe in you!")
+
